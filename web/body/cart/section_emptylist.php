3bc54a4145398fbc92ac7db0aa8ec217ad710984
Satria purnama <purnamasatria91@gmail.com>
2018-09-11 15:52:44 +0700

    deleted cart design

--- web/body/cart/section_emptylist.php
+++
@@ -1,9 +0,0 @@
-<div id="cart-emptylist">
-    <div class="grid-cart-header">
-    	Keranjang Belanja
-    </div>
-    <div class="grid-cart-body"></div>
-    <div class="grid-cart-footer">
-    	Keranjang belanja kamu masih kosong.
-    </div>
-</div>
